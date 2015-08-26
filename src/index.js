1f3e613d761aa9aae24dbeb2502036ffa4cc2e4e
Kuzya <alexkuz@gmail.com>
2015-08-26 23:33:59 +0300

    react-0.14 compatible

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ export ButtonLink from './ButtonLink';
 export ListGroupItemLink from './ListGroupItemLink';
 export MenuItemLink from './MenuItemLink';
 export NavItemLink from './NavItemLink';
-export RouterOverlayTrigger from './RouterOverlayTrigger';
